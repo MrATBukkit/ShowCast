5b2ce8cbdb5c7941dad4ee406e3db5c6b48a9629
Hermann <hewalzl999@gmail.com>
2018-03-03 22:42:16 +0100

    Beispiele Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    title = 'app';
+    divIf = false;
+    names: string[] = ['Hermann', 'Sweety', 'Florian'];
+    ngOnInit(): void {
+      setTimeout(() => {
+          this.title = 'this is a test';
+          this.divIf = true;
+          this.names.push('Alex');
+      }, 10000);
+    }
 }
